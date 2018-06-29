8c0364280a2c37c03f1249dc0f75a064e8333234
foxniver <32783730+foxniver@users.noreply.github.com>
2018-06-29 16:29:36 -0500

    Adding lab4.

---
+++ Project_1/labs/lab4_SRevin/lab4.js
@@ -0,0 +1,57 @@
+function init() {
+
+    var btnInline1 = document.getElementById("average");
+    var btnInline2 = document.getElementById("good");
+    var btnInline3 = document.getElementById("excellent");
+
+    var btnListener1 = document.getElementById("tall");
+    var btnListener2 = document.getElementById("averageHeight");
+    var btnListener3 = document.getElementById("short");
+
+    btnInline1.onclick = dotNotation;
+    btnInline2.onclick = dotNotation;
+    btnInline3.onclick = dotNotation;
+
+    if (window.addEventListener) {
+        btnListener1.addEventListener("click", w3c, false);
+        btnListener2.addEventListener("click", w3c, false);
+        btnListener3.addEventListener("click", w3c, false);
+
+    } else {
+
+        btnListener1.attachEvent("onclick", w3c);
+        btnListener2.attachEvent("onclick", w3c);
+        btnListener3.attachEvent("onclick", w3c);
+    }
+
+
+
+}
+
+function inline(value) {
+
+    console.log("Value for inline: " + value);
+
+}
+
+function dotNotation() {
+
+    console.log("Value for dotNotation: " + this.value);
+
+}
+
+function w3c() {
+
+    var control;
+
+    if (window.event) {
+
+        control = window.event.srcElement;
+
+    } else {
+
+        control = this;
+    }
+
+    console.log("Value for w3c: " + control.value);
+}
