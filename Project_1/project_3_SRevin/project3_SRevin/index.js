8a44d48669c7a4f9d007118e5bcd35ac16664443
foxniver <32783730+foxniver@users.noreply.github.com>
2018-07-27 21:45:30 -0500

    Rearranging functions into a different order.

--- Project_1/project_3_SRevin/project3_SRevin/index.js
+++ Project_1/project_3_SRevin/project3_SRevin/index.js
@@ -1,37 +1,5 @@
 $(document).ready(function() {
 
-    function removeErrors() {
-
-        $(".error").remove();
-    }
-
-    function removeNoTasksMessage() {
-
-        $(".noTasksMessage").remove();
-    }
-
-
-    function appendNoTasksMessage() {
-
-        $("body").append("<p class='noTasksMessage'>You have no tasks to do. Click on the \"Add task\" button to add a task.</p>");
-
-    }
-
-    function validateUserInput(description) {
-
-        console.log("desc: " + description);
-        removeErrors();
-
-        if (description === null || description === "" || description.length > 30) {
-
-            $("#taskInput").before("<sup class='error'> The task cannot be blank or more than 30 characters long.<br/></sup>");
-            return false;
-
-        }
-        return true;
-    }
-
-
     removeErrors();
 
     var table = $("<table id='taskList'></table>");
@@ -57,50 +25,6 @@ $(document).ready(function() {
     }, "xml");
 
 
-    function checkForNoTaskMessage() {
-
-        console.log("tasks length: " + $(".task").length);
-        if ($(".task").length === 0) {
-
-            appendNoTasksMessage();
-
-        } else {
-
-            removeNoTasksMessage();
-        }
-
-    }
-
-
-    function deleteTask(idInput) {
-
-        var id = parseInt(idInput);
-
-        console.log("id: " + id);
-
-        $.ajax({
-            url: "/project3_SRevin/ToDo/deleteTask.php",
-            data: {"id":id},
-            success: function(rowsDeleted) {
-
-                        console.log("rows deleted: " + rowsDeleted);
-
-                        removeTaskFromPage(id);
-
-                     },
-            error: function(request, status, error) {
-
-                        console.log("error: " + error);
-                        console.log("status: " + status);
-
-                        displayWarning(error, id);
-
-                    }
-
-        });
-
-    }
-
 
     $("#addTask").click(function() {
 
@@ -111,54 +35,133 @@ $(document).ready(function() {
 
     })
 
-
-    function addTask(description) {
-
-
-        $.get("/project3_SRevin/ToDo/addTask.php", {"description":description}, function(task) {
-
-            console.log("task added id: " + task.id);
-            removeNoTasksMessage();
-            addTaskRow(task.id, description);
-
-        }, "json")
-
-    }
-
-    function addTaskRow(id, text) {
-
-        $("#taskList").append($("<tr id='" + id + "' class='task'></tr>"));
-        $("tr").last().append("<td></td>");
-        $("td").last().append(text);
-        $("tr").last().append("<td></td>");
-        $("td").last().append("<img src='images/trashCan.png' alt='trash can icon' class='deleteButton' id='delete_" + id + "'>");
-
-        $("#delete_" + id).click(function() {
-
-            deleteTask(id);
-          });
-
-
-    }
-
-    function removeTaskFromPage(id) {
-
-
-        $("tr#" + id).remove();
-
-        if ($(".task").length === 0) {
-
-            appendNoTasksMessage();
-        }
-
-    }
-
-    function displayWarning(status, id) {
-
-        removeErrors();
-        $("#" + id).append("<td class='error'> The task was not deleted. The following error has occurred: "
-                + status + "</td>");
-
-    }
 })
+
+function removeErrors() {
+
+    $(".error").remove();
+}
+
+function removeNoTasksMessage() {
+
+    $(".noTasksMessage").remove();
+}
+
+
+function appendNoTasksMessage() {
+
+    $("body").append("<p class='noTasksMessage'>You have no tasks to do. Click on the \"Add task\" button to add a task.</p>");
+
+}
+
+function validateUserInput(description) {
+
+    console.log("desc: " + description);
+    removeErrors();
+
+    if (description === null || description === "" || description.length > 30) {
+
+        $("#taskInput").before("<sup class='error'> The task cannot be blank or more than 30 characters long.<br/></sup>");
+        return false;
+
+    }
+    return true;
+}
+
+
+function checkForNoTaskMessage() {
+
+    console.log("tasks length: " + $(".task").length);
+    if ($(".task").length === 0) {
+
+        appendNoTasksMessage();
+
+    } else {
+
+        removeNoTasksMessage();
+    }
+
+}
+
+
+function deleteTask(idInput) {
+
+    var id = parseInt(idInput);
+
+    console.log("id: " + id);
+
+    $.ajax({
+        url: "/project3_SRevin/ToDo/deleteTask.php",
+        data: {"id":id},
+        success: function(rowsDeleted) {
+
+                    console.log("rows deleted: " + rowsDeleted);
+
+                    removeTaskFromPage(id);
+
+                 },
+        error: function(request, status, error) {
+
+                    console.log("error: " + error);
+                    console.log("status: " + status);
+
+                    displayWarning(error, id);
+
+                }
+
+    });
+
+}
+
+
+
+function addTask(description) {
+
+
+    $.get("/project3_SRevin/ToDo/addTask.php", {"description":description}, function(task) {
+
+        console.log("task added id: " + task.id);
+        removeNoTasksMessage();
+        addTaskRow(task.id, description);
+
+    }, "json")
+
+}
+
+function addTaskRow(id, text) {
+
+    $("#taskList").append($("<tr id='" + id + "' class='task'></tr>"));
+    $("tr").last().append("<td></td>");
+    $("td").last().append(text);
+    $("tr").last().append("<td></td>");
+    $("td").last().append("<img src='images/trashCan.png' alt='trash can icon' class='deleteButton' id='delete_" + id + "'>");
+
+    $("#delete_" + id).click(function() {
+
+        deleteTask(id);
+      });
+
+
+}
+
+function removeTaskFromPage(id) {
+
+
+    $("tr#" + id).remove();
+
+    if ($(".task").length === 0) {
+
+        appendNoTasksMessage();
+    }
+
+}
+
+function displayWarning(status, id) {
+
+    removeErrors();
+    $("#" + id).append("<td class='error'> The task was not deleted. The following error has occurred: "
+            + status + "</td>");
+
+}
+
 
